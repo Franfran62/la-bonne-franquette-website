f5541b29c402bcf7137354f635d19e4e241674b4
Thomas Dignoire <tdignoire@protonmail.com>
2025-03-20 16:10:23 +0100

    💄 Modification du théme

--- src/main.js
+++ src/main.js
@@ -8,6 +8,20 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
+import colors from "tailwindcss/colors.js";
+
+const franquetteTheme = {
+    dark: false,
+    colors: {
+        primary: '#EDD382',
+        secondary: '#020122',
+        accent: '#FC9E4F',
+        error: colors.red['600'],
+        info: colors.teal['400'],
+        success: colors.green['600'],
+        warning: colors.amber['500'],
+    },
+}
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -15,6 +29,12 @@ const vuetify = createVuetify({
     components,
     directives,
     iconfont: 'mdi',
+    theme: {
+        defaultTheme: 'franquetteTheme',
+        themes: {
+            franquetteTheme,
+        },
+    },
   })
 
 app.use(pinia);
