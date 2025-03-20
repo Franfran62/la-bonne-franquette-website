96cd072182d14356f6cff042589b25213092d75d
Thomas Dignoire <tdignoire@protonmail.com>
2025-03-20 16:15:47 +0100

    ✨ Ajout d'un nouveau thème Franquette et mise à jour de l'importation dans main.js

--- src/main.js
+++ src/main.js
@@ -8,20 +8,9 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
-import colors from "tailwindcss/colors.js";
+import franquetteTheme from "@/themes/franquetteTheme.js";
 
-const franquetteTheme = {
-    dark: false,
-    colors: {
-        primary: '#EDD382',
-        secondary: '#020122',
-        accent: '#FC9E4F',
-        error: colors.red['600'],
-        info: colors.teal['400'],
-        success: colors.green['600'],
-        warning: colors.amber['500'],
-    },
-}
+
 
 const app = createApp(App);
 const pinia = createPinia();
