4b753931348c8719d47111333b7f08977b1cf58c
Thomas Dignoire <tdignoire@protonmail.com>
2025-03-31 20:41:30 +0200

    ✨ Ajout de la gestion responsive pour le composant d'équipe

--- src/components/dashboard/TeamComponent.vue
+++ src/components/dashboard/TeamComponent.vue
@@ -6,6 +6,7 @@ import ErrorInfo from "@/components/snackbars/ErrorInfo.vue";
 import SuccessInfo from "@/components/snackbars/SuccessInfo.vue";
 import HintInfo from "@/components/snackbars/HintInfo.vue";
 import AlertDeleteUserDialog from "@/components/dialogs/AlertDeleteUserDialog.vue";
+import {useDisplay} from "vuetify";
 
 const snackbarError = ref(false);
 const snackbarSuccess = ref(false);
@@ -26,6 +27,11 @@ const selectedUser = ref(null);
 const selectedRole = ref(null);
 const formTitle = ref("Ajouter un nouveau membre");
 const buttonTitle = ref("Ajouter");
+const {xs, sm, md, lg, xl} = useDisplay();
+
+const isMobile = computed(() => xs.value || sm.value);
+const isTablet = computed(() => md.value);
+const isDesktop = computed(() => lg.value || xl.value);
 
 const availableRoles = [
   {value: "ROLE_USER", name: "Utilisateur"},
@@ -131,37 +137,43 @@ const clearCurrentUser = () => {
 </script>
 
 <template>
-  <div class="flex justify-center mt-16">
-    <v-card min-width="700" variant="text" class="px-8">
-      <v-card-title>Gestion des membres de l'équipe</v-card-title>
-      <v-card-actions class="flex justify-start">
-        <v-btn base-color="success" rounded="xl" variant="elevated" class="pr-4" prepend-icon="mdi-plus"
-               @click="clearCurrentUser">
-          <span class="whiteText">Ajouter un membre</span>
-        </v-btn>
-        <span class="ml-16">Trier par role : </span>
-        <v-select label="Role" :items="availableRoles" v-model="selectedRole" item-title="name" item-value="value"
-                  clearable variant="outlined" density="compact" color="accent" rounded="xl" width="225"
-                  class="mt-6"/>
-        <v-btn @click="refreshUsers" icon="mdi-refresh" variant="text"></v-btn>
-      </v-card-actions>
-      <v-list>
-        <v-list-item v-for="(u, i) in filterByRole" :key="i" color="primary" base-color="primary" rounded="lg"
-                     variant="elevated" min-height="32" height="32" class="my-2 mx-2" @click="handleSelectedUser(u)">
-          <v-list-item-title>{{ u.username }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
+  <v-card variant="text" class="flex justify-center mx-auto">
+    <v-card-title>Gestion des membres de l'équipe</v-card-title>
+  </v-card>
+  <div :class="{'flex flex-column-reverse': isMobile, 'flex justify-center': !isMobile}">
+    <v-card :width="isMobile ? 400 : 700" variant="text" :class="{'mx-auto': isMobile }">
+      <v-card-title v-show="!isMobile">Membres de l'équipe</v-card-title>
+      <div>
+        <v-card-actions :class="{'flex justify-start': !isMobile}">
+          <v-btn base-color="success" rounded="xl" variant="elevated" class="pr-4" prepend-icon="mdi-plus"
+                 @click="clearCurrentUser" v-show="!isMobile">
+            <span class="whiteText">Ajouter un membre</span>
+          </v-btn>
+          <span class="ml-14" v-show="!isMobile">Trier par role : </span>
+          <v-select label="Role" :items="availableRoles" v-model="selectedRole" item-title="name" item-value="value"
+                    clearable variant="outlined" density="compact" color="accent" rounded="xl" :width="!isMobile ? 225: 25"
+                    class="mt-6"/>
+          <v-btn @click="clearCurrentUser" icon="mdi-plus" variant="text" base-color="success" rounded="xl" v-show="isMobile" ></v-btn>
+          <v-btn @click="refreshUsers" icon="mdi-refresh" variant="text"></v-btn>
+        </v-card-actions>
+        <v-list>
+          <v-list-item v-for="(u, i) in filterByRole" :key="i" color="primary" base-color="primary" rounded="lg"
+                       variant="elevated" min-height="32" height="32" class="my-2 mx-2" @click="handleSelectedUser(u)">
+            <v-list-item-title>{{ u.username }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </div>
     </v-card>
 
-    <v-card min-width="700" variant="text" class="px-8">
+    <v-card :width="isMobile ? 450 : 700" variant="text" :class="{'px-8': !isMobile, 'px-8 mx-auto': isMobile }">
       <v-card-title>{{ formTitle }}</v-card-title>
       <v-card-actions class="flex justify-start">
-        <v-btn base-color="delete" rounded="xl" variant="elevated" prepend-icon="mdi-delete" class="pr-4 mt-6"
+        <v-btn base-color="delete" rounded="xl" variant="elevated" prepend-icon="mdi-delete" :class="{'pr-4 mt-8':!isMobile, 'pr-4':isMobile}"
                @click="handleDeleteSubmit">
           <span class="whiteText">Supprimer</span>
         </v-btn>
       </v-card-actions>
-      <v-form v-model="valid" class="my-10 mx-2" validate-on="invalid-input" @submit.prevent="handleSubmit">
+      <v-form v-model="valid" :class="{'my-10 mx-2': !isMobile, 'my-2 mx-2': isMobile}" validate-on="invalid-input" @submit.prevent="handleSubmit">
         <v-select label="Role" :items="availableRoles" v-model="newRole" item-title="name" item-value="value"
                   variant="outlined" density="compact" color="accent" :rules="[v => !!v || 'le role est nécessaire']"
                   rounded="xl" class="input-spacing"/>
@@ -169,39 +181,39 @@ const clearCurrentUser = () => {
                       variant="outlined" :rules="[v => !!v || 'Le nom d\'utilisateur est nécessaire']" required
                       rounded="xl" density="compact" class="input-spacing" color="accent"/>
         <span v-if="selectedUser && selectedUser.value !== null">
-          <v-text-field v-model="oldPassword" label="Ancient mot de passe"
-                        placeholder="Entrez l'ancient mot de passe" variant="outlined"
-                        :rules="[v => !!v || 'Le mot de passe est nécessaire']" required
-                        rounded="xl" density="compact" class="input-spacing" color="accent"
-                        :append-inner-icon="!visible ? 'mdi-eye-off' : 'mdi-eye'"
-                        :type="visible ? 'text' : 'password'"
-                        @click:append-inner="visible = !visible"/>
-          <v-text-field v-model="password" label="Nouveau mot de passe"
-                        placeholder="Entrez le nouveau mot de passe" variant="outlined"
-                        :rules="[v => !!v || 'Le mot de passe est nécessaire', v => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(v) || 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre']"
-                        required rounded="xl" density="compact" class="input-spacing"
-                        color="accent" :append-inner-icon="!visible ? 'mdi-eye-off' : 'mdi-eye'"
-                        :type="visible ? 'text' : 'password'"
-                        @click:append-inner="visible = !visible"/>
-        </span>
+                <v-text-field v-model="oldPassword" label="Ancient mot de passe"
+                              placeholder="Entrez l'ancient mot de passe" variant="outlined"
+                              :rules="[v => !!v || 'Le mot de passe est nécessaire']" required
+                              rounded="xl" density="compact" class="input-spacing" color="accent"
+                              :append-inner-icon="!visible ? 'mdi-eye-off' : 'mdi-eye'"
+                              :type="visible ? 'text' : 'password'"
+                              @click:append-inner="visible = !visible"/>
+                <v-text-field v-model="password" label="Nouveau mot de passe"
+                              placeholder="Entrez le nouveau mot de passe" variant="outlined"
+                              :rules="[v => !!v || 'Le mot de passe est nécessaire', v => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(v) || 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre']"
+                              required rounded="xl" density="compact" class="input-spacing"
+                              color="accent" :append-inner-icon="!visible ? 'mdi-eye-off' : 'mdi-eye'"
+                              :type="visible ? 'text' : 'password'"
+                              @click:append-inner="visible = !visible"/>
+              </span>
         <span v-else>
-          <v-text-field v-model="password" label="Mot de passe"
-                        placeholder="Entrez le mot de passe" variant="outlined"
-                        :rules="[v => !!v || 'Le mot de passe est nécessaire', v => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(v) || 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre']"
-                        required rounded="xl" density="compact" class="input-spacing"
-                        color="accent" :append-inner-icon="!visible ? 'mdi-eye-off' : 'mdi-eye'"
-                        :type="visible ? 'text' : 'password'"
-                        @click:append-inner="visible = !visible"/>
-          <v-text-field v-model="passwordTest" label="Retaper votre mot de passe"
-                        placeholder="Retaper votre mot de passe" variant="outlined"
-                        :rules="[v => (v === password) || 'Les mots de passe sont différents']"
-                        required rounded="xl"
-                        :append-inner-icon="!visible ? 'mdi-eye-off' : 'mdi-eye'"
-                        :type="visible ? 'text' : 'password'"
-                        @click:append-inner="visible = !visible" density="compact"
-                        class="input-spacing" color="accent"/>
-        </span>
-        <div class="d-flex justify-center pb-8">
+                <v-text-field v-model="password" label="Mot de passe"
+                              placeholder="Entrez le mot de passe" variant="outlined"
+                              :rules="[v => !!v || 'Le mot de passe est nécessaire', v => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(v) || 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre']"
+                              required rounded="xl" density="compact" class="input-spacing"
+                              color="accent" :append-inner-icon="!visible ? 'mdi-eye-off' : 'mdi-eye'"
+                              :type="visible ? 'text' : 'password'"
+                              @click:append-inner="visible = !visible"/>
+                <v-text-field v-model="passwordTest" label="Retaper votre mot de passe"
+                              placeholder="Retaper votre mot de passe" variant="outlined"
+                              :rules="[v => (v === password) || 'Les mots de passe sont différents']"
+                              required rounded="xl"
+                              :append-inner-icon="!visible ? 'mdi-eye-off' : 'mdi-eye'"
+                              :type="visible ? 'text' : 'password'"
+                              @click:append-inner="visible = !visible" density="compact"
+                              class="input-spacing" color="accent"/>
+              </span>
+        <div :class="{'d-flex justify-center pb-8': !isMobile, 'd-flex justify-center': isMobile}">
           <v-btn type="submit" color="primary" rounded="xl" size="large">
             <div class="justify-start font-semibold">{{ buttonTitle }}</div>
           </v-btn>
