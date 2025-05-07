f9160162dda5dbd3e68a7b236957fe30b42621de
Thomas Dignoire <tdignoire@protonmail.com>
2025-05-07 18:08:53 +0200

    ✨ Ajout de la gestion des plages de dates pour filtrer les commandes

--- src/components/dashboard/OrderComponent.vue
+++ src/components/dashboard/OrderComponent.vue
@@ -6,9 +6,14 @@ import {fetchOrders} from "@/services/orderService.js";
 import Order from "@/model/Order.js";
 import {useDisplay} from "vuetify";
 import ErrorInfo from "@/components/snackbars/ErrorInfo.vue";
+import DateRange from "@/model/DateRange.js";
+
+const today = new Date();
 
 const snackbarError = ref(false);
 const errorText = ref("");
+
+const selectedDateRange = ref(DateRange.ALL);
 
 const orders = ref([]);
 const selectedOrder = ref(null);
@@ -18,8 +23,21 @@ onBeforeMount(() => refreshOrders());
 
 const isMobile = computed(() => xs.value || sm.value);
 
+function getWeekNumber(date) {
+  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+  const pastDaysOfYear = (date - firstDayOfYear + (firstDayOfYear.getDay() || 7) * 86400000) / 86400000;
+  return Math.ceil(pastDaysOfYear / 7);
+}
+
+
 const onFilter = computed(() => {
-  return orders.value;
+  switch (selectedDateRange.value) {
+    case DateRange.ALL : return orders.value;
+    case DateRange.TODAY : return orders.value.filter(order => (order.dateSaisie.getDate() === today.getDate() && order.dateSaisie.getMonth() === today.getMonth() && order.dateSaisie.getFullYear() === today.getFullYear()));
+    case DateRange.WEEK : return orders.value.filter(order => (getWeekNumber(order.dateSaisie) === getWeekNumber(today)));
+    case DateRange.MONTH : return orders.value.filter(order => (order.dateSaisie.getMonth() === today.getMonth() && order.dateSaisie.getFullYear() === today.getFullYear()));
+    case DateRange.YEAR : return orders.value.filter(order => (order.dateSaisie.getFullYear() === today.getFullYear()));
+  }
 })
 
 const refreshOrders = async () => {
@@ -28,10 +46,9 @@ const refreshOrders = async () => {
     if (response.status !== 200) throw new Error(response.message);
     console.log("Response : ")
     console.log(response.data);
-    orders.value = response.data.map(order => new Order(order.commandeId, order.numero, order.prixTTC, order.surPlace, order.status, order.paiementTypeCommande, order.dateSaisie, order.dateLivraison, order.articles, order.nbArticle, order.menus, order.paiementSet, order.paye));
+    orders.value = response.data.map(order => new Order(order.commandeId, order.numero, order.prixTTC, order.surPlace, order.status, order.paiementTypeCommande, new Date(order.dateSaisie), order.dateLivraison ? new Date(order.dateLivraison) : null, order.articles, order.nbArticle, order.menus, order.paiementSet, order.paye));
     selectedOrder.value = null;
   } catch (e) {
-    console.log(e);
     errorText.value = e.message;
     snackbarError.value = true;
   }
@@ -47,12 +64,27 @@ const refreshOrders = async () => {
     <v-card :width="isMobile ? 400 : 700" variant="text" :class="{'mx-auto': isMobile }">
       <v-card-title v-show="!isMobile">Résumé des commandes</v-card-title>
       <div>
-        <v-card-actions :class="{'flex justify-start': !isMobile}">
-          <span class="ml-14" v-show="!isMobile">Trier par : </span>
+<!--        <div class="d-flex flex-column align-center">
+
+        </div>-->
+        <v-card-actions :class="{'flex justify-space-between': !isMobile}">
+<!--          <span class="ml-14" v-show="!isMobile">Trier par : </span>
           <v-select label="Role" :items="availableRoles" v-model="selectedRole" item-title="name" item-value="value"
                     clearable variant="outlined" density="compact" color="accent" rounded="xl"
                     :width="!isMobile ? 225: 25"
-                    class="mt-6"/>
+                    class="mt-6"/>-->
+          <v-btn-toggle
+              v-model="selectedDateRange"
+              color="primary"
+              mandatory
+              variant=flat
+          >
+            <v-btn :value="DateRange.ALL" rounded="lg">{{ DateRange.ALL }}</v-btn>
+            <v-btn  :value="DateRange.TODAY" rounded="lg">{{ DateRange.TODAY }}</v-btn>
+            <v-btn  :value="DateRange.WEEK" rounded="lg">{{ DateRange.WEEK }}</v-btn>
+            <v-btn  :value="DateRange.MONTH" rounded="lg">{{ DateRange.MONTH }}</v-btn>
+            <v-btn  :value="DateRange.YEAR" rounded="lg">{{ DateRange.YEAR }}</v-btn>
+          </v-btn-toggle>
           <v-btn @click="refreshOrders" icon="mdi-refresh" variant="text"></v-btn>
         </v-card-actions>
         <OrderListComponent :on-filter="() => onFilter" :on-select="() => {}" :on-delete="() => {}"/>
---
+++ src/model/DateRange.js
@@ -0,0 +1,9 @@
+const DateRange = Object.freeze({
+    ALL: "Tout",
+    TODAY: "Aujourd'hui",
+    WEEK: "Cette semaine",
+    MONTH: "Ce mois",
+    YEAR: "Cette année",
+});
+
+export default DateRange;
